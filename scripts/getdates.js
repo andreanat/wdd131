af0862d3ae7143d4e10d757f5143781b5d64450f
andreanat <andrea.nmb13@gmail.com>
2025-07-09 10:48:04 -0600

    yes

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
